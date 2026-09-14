11522388b81181b3d81088181ae3d7bcd9ea4ff4
Mateus Ristow <mateus.ristow@aluno.senai.br>
2026-09-14 17:57:43 +0000

    Preparar backend para deploy

--- backend/index.js
+++ backend/index.js
@@ -127,6 +127,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
+})
---
+++ backend/prisma/migrations/20260914174720_adicionar_dimensoes_produto/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Produtos" ADD COLUMN "altura" REAL;
+ALTER TABLE "Produtos" ADD COLUMN "comprimento" REAL;
+ALTER TABLE "Produtos" ADD COLUMN "largura" REAL;
+ALTER TABLE "Produtos" ADD COLUMN "peso" REAL;
